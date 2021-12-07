41cda69eb79c8f5fda51f69b6af956054046f1ab
RdotSilva <RyandotSilva@gmail.com>
2021-12-07 20:23:18 +0000

    Add addCorsMappings function

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/WebConfig.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/WebConfig.kt
@@ -1,10 +1,21 @@
 package com.rdotsilva.mockproductrestapi
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
+/**
+ * Configuration class used for the web server
+ */
 class WebConfig: WebMvcConfigurer {
+    /**
+     * Enable CORS so we can use this service on the frontend
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:4200")
+    }
 }
