7e1a0a8a4418fc212934a02d88740f75222f8c4f
RdotSilva <RyandotSilva@gmail.com>
2021-12-07 20:08:43 +0000

    Initial commit

---
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/MockProductRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.rdotsilva.mockproductrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MockProductRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MockProductRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/rdotsilva/mockproductrestapi/MockProductRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rdotsilva.mockproductrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MockProductRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
