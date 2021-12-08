b42c59c043531e881cee4c9d116a40cafc90c5c0
RdotSilva <RyandotSilva@gmail.com>
2021-12-08 19:25:05 +0000

    Add allProducts function TODO

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
@@ -1,4 +1,18 @@
 package com.rdotsilva.mockproductrestapi.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api")
 class ProductController {
+
+    /**
+     * Get request to fetch all products
+     */
+    @GetMapping("/products")
+    fun allProducts() {
+        // TODO: Implement get all products
+    }
 }
