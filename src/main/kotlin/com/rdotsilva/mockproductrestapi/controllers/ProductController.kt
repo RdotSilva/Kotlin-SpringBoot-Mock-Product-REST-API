d310f5d3a91a2d5ea13ec161ec3b25adea71b389
RdotSilva <RyandotSilva@gmail.com>
2021-12-08 19:30:56 +0000

    Add logic to fetch all products

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
@@ -1,18 +1,21 @@
 package com.rdotsilva.mockproductrestapi.controllers
 
+import com.rdotsilva.mockproductrestapi.models.Product
+import com.rdotsilva.mockproductrestapi.repositories.ProductRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api")
-class ProductController {
+class ProductController(private val productRepository: ProductRepository) {
 
     /**
      * Get request to fetch all products
      */
     @GetMapping("/products")
-    fun allProducts() {
-        // TODO: Implement get all products
+    fun allProducts(): ResponseEntity<List<Product>> {
+        return ResponseEntity.ok(this.productRepository.findAll())
     }
 }
