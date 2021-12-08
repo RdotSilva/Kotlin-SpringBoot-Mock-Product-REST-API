11ed831532a41d4fdb21eea19b520dbf26287384
RdotSilva <RyandotSilva@gmail.com>
2021-12-08 19:21:55 +0000

    Initialize product properties

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Product.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Product.kt
@@ -1,4 +1,14 @@
 package com.rdotsilva.mockproductrestapi.models
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
 class Product {
+    @Id
+    var _id = ""
+    var title = ""
+    var description = ""
+    var image = ""
+    var price = 0
 }
