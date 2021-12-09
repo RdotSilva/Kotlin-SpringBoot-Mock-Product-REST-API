d1699723b25a104ff87eb3bf0e0e73b81af0dd7b
RdotSilva <RyandotSilva@gmail.com>
2021-12-09 20:08:55 +0000

    Add logic to create random mock products

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/DataSeeder.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/DataSeeder.kt
@@ -1,4 +1,20 @@
 package com.rdotsilva.mockproductrestapi.utils
 
-class DataSeeder {
+import com.rdotsilva.mockproductrestapi.models.Product
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+import kotlin.random.Random
+
+@Component
+class DataSeeder: ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        for (i in 1..50) {
+            val product = Product()
+            product.title = "Title # $i"
+            product.description = "Description # ${i + 1}"
+            product.image = "http://lorempixel.com/200/200?=${Random.nextInt(10000)}"
+            product.price = Random.nextInt(10, 100)
+        }
+    }
 }
