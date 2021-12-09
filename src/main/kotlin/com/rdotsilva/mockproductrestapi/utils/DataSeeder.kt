d9bdb3c21ee52e4f82a5b7f1bced7541d7b36a08
RdotSilva <RyandotSilva@gmail.com>
2021-12-09 20:10:27 +0000

    Update logic to seed products into database

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/DataSeeder.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/DataSeeder.kt
@@ -1,13 +1,17 @@
 package com.rdotsilva.mockproductrestapi.utils
 
 import com.rdotsilva.mockproductrestapi.models.Product
+import com.rdotsilva.mockproductrestapi.repositories.ProductRepository
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 import kotlin.random.Random
 
 @Component
-class DataSeeder: ApplicationRunner {
+class DataSeeder(private val productRepository: ProductRepository): ApplicationRunner {
+    /**
+     * Generate 50 and seed 50 random products
+     */
     override fun run(args: ApplicationArguments?) {
         for (i in 1..50) {
             val product = Product()
@@ -15,6 +19,7 @@ class DataSeeder: ApplicationRunner {
             product.description = "Description # ${i + 1}"
             product.image = "http://lorempixel.com/200/200?=${Random.nextInt(10000)}"
             product.price = Random.nextInt(10, 100)
+            this.productRepository.save(product)
         }
     }
 }
