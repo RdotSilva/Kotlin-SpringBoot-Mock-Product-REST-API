fc3ba1529db9e37f23a71de72656c5f6da958cdf
RdotSilva <RyandotSilva@gmail.com>
2021-12-11 11:35:00 +0000

    Refactor Product model

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Product.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Product.kt
@@ -1,14 +1,9 @@
 package com.rdotsilva.mockproductrestapi.models
 
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document()
-class Product {
-    @Id
-    var id = ""
-    var title = ""
-    var description = ""
-    var image = ""
-    var price = 0
+class Product(@Id val _id: ObjectId = ObjectId.get(), val title: String, val description: String, val image: String, val price: Int) {
 }
