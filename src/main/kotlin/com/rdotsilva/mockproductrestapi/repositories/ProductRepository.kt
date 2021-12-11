0202750e0570565edd908c099fb86916e0d72b46
RdotSilva <RyandotSilva@gmail.com>
2021-12-11 11:51:40 +0000

    Add searchByTitle function

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/ProductRepository.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/ProductRepository.kt
@@ -2,6 +2,10 @@ package com.rdotsilva.mockproductrestapi.repositories
 
 import com.rdotsilva.mockproductrestapi.models.Product
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
 interface ProductRepository : MongoRepository<Product, String> {
+
+    @Query("{'title': {\$regex: ?0, \$options: 'i' }}")
+    fun searchByTitle(searchQuery: String): List<Product>
 }
