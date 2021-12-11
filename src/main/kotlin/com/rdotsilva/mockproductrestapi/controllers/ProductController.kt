0d6db8393e38e1bae234175c3fea295cad1cb522
RdotSilva <RyandotSilva@gmail.com>
2021-12-11 11:56:35 +0000

    Add getProductByTitle controller

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
@@ -2,9 +2,11 @@ package com.rdotsilva.mockproductrestapi.controllers
 
 import com.rdotsilva.mockproductrestapi.models.Product
 import com.rdotsilva.mockproductrestapi.repositories.ProductRepository
+import org.springframework.boot.context.properties.bind.DefaultValue
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,6 +18,11 @@ class ProductController(private val productRepository: ProductRepository) {
      */
     @GetMapping("/products")
     fun allProducts(): ResponseEntity<List<Product>> {
+        return ResponseEntity.ok(this.productRepository.findAll())
+    }
+
+    @GetMapping("/products")
+    fun getProductByTitle(@RequestParam("title", defaultValue = "") title: String,): ResponseEntity<List<Product>> {
         return ResponseEntity.ok(this.productRepository.findAll())
     }
 }
