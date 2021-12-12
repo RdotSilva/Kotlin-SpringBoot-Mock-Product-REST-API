0ba75ce3b69131476aa655c2e60b65d681963658
RdotSilva <RyandotSilva@gmail.com>
2021-12-12 13:25:58 +0000

    Add getAllCustomers function

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/CustomerController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/CustomerController.kt
@@ -1,8 +1,22 @@
 package com.rdotsilva.mockproductrestapi.controllers
 
+import com.rdotsilva.mockproductrestapi.models.Customer
+import com.rdotsilva.mockproductrestapi.models.Product
+import com.rdotsilva.mockproductrestapi.repositories.CustomerRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api")
-class CustomerController
+class CustomerController(private val customerRepository: CustomerRepository) {
+
+    /**
+     * Get request to fetch all customers
+     */
+    @GetMapping("/customers")
+    fun getAllCustomers(): ResponseEntity<List<Customer>> {
+        return ResponseEntity.ok(this.customerRepository.findAll())
+    }
+}
