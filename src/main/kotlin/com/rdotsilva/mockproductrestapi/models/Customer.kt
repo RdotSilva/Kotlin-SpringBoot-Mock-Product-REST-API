a0a961a087cd2dae311d0cd9ad3118a153c12002
RdotSilva <RyandotSilva@gmail.com>
2021-12-12 13:14:55 +0000

    Add properties for Customer model

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Customer.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/models/Customer.kt
@@ -1,3 +1,8 @@
 package com.rdotsilva.mockproductrestapi.models
 
-class Customer
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+class Customer(@Id val _id: ObjectId = ObjectId.get(), var firstName: String, var lastName: String)
