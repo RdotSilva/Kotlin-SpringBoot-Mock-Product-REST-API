53d1f8f39ca8e958d7d01817b3af0bf522947fe6
RdotSilva <RyandotSilva@gmail.com>
2021-12-12 13:14:05 +0000

    Add kotlin-faker dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("io.github.serpro69:kotlin-faker:1.9.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
