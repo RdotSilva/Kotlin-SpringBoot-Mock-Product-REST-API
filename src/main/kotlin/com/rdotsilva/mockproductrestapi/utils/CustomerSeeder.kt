976fa9c044590b350075c8278ebea50b5895dab9
RdotSilva <RyandotSilva@gmail.com>
2021-12-12 13:21:21 +0000

    Add override for main runner

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/CustomerSeeder.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/utils/CustomerSeeder.kt
@@ -1,9 +1,18 @@
 package com.rdotsilva.mockproductrestapi.utils
 
+import com.rdotsilva.mockproductrestapi.models.Customer
 import com.rdotsilva.mockproductrestapi.repositories.CustomerRepository
+import io.github.serpro69.kfaker.Faker
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
 @Component
-class CustomerSeeder(private val customerRepository: CustomerRepository): ApplicationRunner
+class CustomerSeeder(private val customerRepository: CustomerRepository): ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        for (i in 1..50) {
+            val customer = createRandomCustomer(i)
+            this.customerRepository.save(customer)
+        }
+    }
+}
