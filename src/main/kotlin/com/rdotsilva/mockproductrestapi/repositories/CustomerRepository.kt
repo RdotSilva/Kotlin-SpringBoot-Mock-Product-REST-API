a38e68840f8dc39c651e8d70944f1a4f69b49ee4
RdotSilva <RyandotSilva@gmail.com>
2021-12-13 18:40:25 +0000

    Add query to fetch all customers on mailing list

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
@@ -2,5 +2,10 @@ package com.rdotsilva.mockproductrestapi.repositories
 
 import com.rdotsilva.mockproductrestapi.models.Customer
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
-interface CustomerRepository : MongoRepository<Customer, String>
+interface CustomerRepository : MongoRepository<Customer, String> {
+
+    @Query("{'onMailingList': ?0}")
+    fun findByOnMailingList(onMailingList: Boolean): List<Customer>
+}
