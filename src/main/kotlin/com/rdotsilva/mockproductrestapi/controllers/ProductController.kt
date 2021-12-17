15239cb4205bb6614116abe337ccc46f28b33840
RdotSilva <RyandotSilva@gmail.com>
2021-12-17 20:29:14 +0000

    Add getProductById get mapping function

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
@@ -1,12 +1,11 @@
 package com.rdotsilva.mockproductrestapi.controllers
 
+import com.rdotsilva.mockproductrestapi.models.Customer
 import com.rdotsilva.mockproductrestapi.models.Product
 import com.rdotsilva.mockproductrestapi.repositories.ProductRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("api/products")
@@ -26,5 +25,13 @@ class ProductController(private val productRepository: ProductRepository) {
     @GetMapping("/search")
     fun getProductByTitle(@RequestParam("title", defaultValue = "") title: String): ResponseEntity<List<Product>> {
         return ResponseEntity.ok(this.productRepository.searchByTitle(title))
+    }
+
+    /**
+     * Get request to fetch product by ID
+     */
+    @GetMapping("/{id}")
+    fun getProductById(@PathVariable id: String): ResponseEntity<Optional<Product>> {
+        return ResponseEntity.ok(this.productRepository.findById(id))
     }
 }
