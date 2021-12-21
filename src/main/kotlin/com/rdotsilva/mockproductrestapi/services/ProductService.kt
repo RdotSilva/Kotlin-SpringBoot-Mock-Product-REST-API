278e9b0e6e4c053bc3dba02a1434f42119ded4fa
RdotSilva <RyandotSilva@gmail.com>
2021-12-21 18:01:48 +0000

    Change ProductService to interface

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/services/ProductService.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/services/ProductService.kt
@@ -1,3 +1,11 @@
 package com.rdotsilva.mockproductrestapi.services
 
-class ProductService
+import com.rdotsilva.mockproductrestapi.models.Product
+
+interface ProductService {
+    fun getAllProducts(): List<Product>
+    fun getProductByTitle(title: String): Product
+    fun getProductById(id: String): Product
+    fun removeProduct(id: String): Unit
+    fun getProductsInStock(inStock: Boolean): List<Product>
+}
