c1e1045fc1334b2184e06e2f0339fb67e8ca95bc
RdotSilva <RyandotSilva@gmail.com>
2021-12-22 18:28:23 +0000

    Update getProductByTitle controller mapping to use service

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/controllers/ProductController.kt
@@ -25,7 +25,7 @@ class ProductController(private val productService: ProductService) {
      */
     @GetMapping("/search")
     fun getProductByTitle(@RequestParam("title", defaultValue = "") title: String): ResponseEntity<List<Product>> {
-        return ResponseEntity.ok(this.productRepository.searchByTitle(title))
+        return ResponseEntity.ok(this.productService.getProductByTitle(title))
     }
 
     /**
