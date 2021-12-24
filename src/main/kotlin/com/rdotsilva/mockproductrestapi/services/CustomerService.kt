61201f8668a13d277a4398893b6f9e0afd55d03a
RdotSilva <RyandotSilva@gmail.com>
2021-12-24 09:20:06 -0500

    Update CustomerService interface

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CustomerService.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CustomerService.kt
@@ -1,4 +1,12 @@
 package com.rdotsilva.mockproductrestapi.services
 
+import com.rdotsilva.mockproductrestapi.models.Customer
+import java.util.*
+
 interface CustomerService {
+    fun getAllCustomers(): MutableList<Customer>
+    fun getAllCustomersOnMailingList(onMailingList: Boolean): List<Customer>
+    fun getCustomerById(id: String): Optional<Customer>
+    fun addCustomer(customer: Customer)
+    fun removeCustomer(id: String)
 }
