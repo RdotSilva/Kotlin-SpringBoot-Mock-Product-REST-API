53fa8e554520f5c75898efb1b234a7adb3592517
RdotSilva <RyandotSilva@gmail.com>
2021-12-24 09:24:33 -0500

    Add searchByName query

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
@@ -1,6 +1,7 @@
 package com.rdotsilva.mockproductrestapi.repositories
 
 import com.rdotsilva.mockproductrestapi.models.Customer
+import com.rdotsilva.mockproductrestapi.models.Product
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 
@@ -8,4 +9,7 @@ interface CustomerRepository : MongoRepository<Customer, String> {
 
     @Query("{'onMailingList': ?0}")
     fun findByOnMailingList(onMailingList: Boolean): List<Customer>
+
+    @Query("{'name': {\$regex: ?0, \$options: 'i' }}")
+    fun searchByName(searchQuery: String):  List<Customer>
 }
