44c9e02cbff4ef9424af6e0f4ef976665fa76504
RdotSilva <RyandotSilva@gmail.com>
2021-12-26 13:38:11 -0500

    Remove unused import

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/repositories/CustomerRepository.kt
@@ -1,7 +1,6 @@
 package com.rdotsilva.mockproductrestapi.repositories
 
 import com.rdotsilva.mockproductrestapi.models.Customer
-import com.rdotsilva.mockproductrestapi.models.Product
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 
