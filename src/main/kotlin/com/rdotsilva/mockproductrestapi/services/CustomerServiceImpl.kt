facb5bbae71009d18c91b1dbb28eee9a0270f721
RdotSilva <RyandotSilva@gmail.com>
2021-12-27 06:52:36 -0500

    Add overrides for CustomerServiceImpl

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CustomerServiceImpl.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CustomerServiceImpl.kt
@@ -6,26 +6,26 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CustomerServiceImpl(private val customerRepository: CustomerRepository) {
+class CustomerServiceImpl(private val customerRepository: CustomerRepository) : CustomerService {
 
     /**
      * Get a list of all customers
      */
-    fun getAllCustomers(): MutableList<Customer> {
+    override fun getAllCustomers(): MutableList<Customer> {
         return this.customerRepository.findAll()
     }
 
     /**
      * Get a list of all customer emails on mailing list
      */
-    fun getAllCustomersOnMailingList(onMailingList: Boolean): List<Customer> {
+    override fun getAllCustomersOnMailingList(onMailingList: Boolean): List<Customer> {
         return this.customerRepository.findByOnMailingList(onMailingList)
     }
 
     /**
      * Get customer by ID
      */
-    fun getCustomerById(id: String): Optional<Customer> {
+    override fun getCustomerById(id: String): Optional<Customer> {
         // Do validation here and throw exception if data is bad -> try/catch inside controller and return correct status and error message
         return this.customerRepository.findById(id)
     }
@@ -33,21 +33,21 @@ class CustomerServiceImpl(private val customerRepository: CustomerRepository) {
     /**
      * Create a new customer
      */
-    fun addCustomer(customer: Customer) {
+    override fun addCustomer(customer: Customer) {
         this.customerRepository.save(customer)
     }
 
     /**
      * Remove an existing customer
      */
-    fun removeCustomer(id: String) {
+    override fun removeCustomer(id: String) {
         this.customerRepository.deleteById(id)
     }
 
     /**
      * Get a list of all customer emails on mailing list
      */
-    fun getCustomersByFirstName(firstName: String): List<Customer> {
-        return this.customerRepository.searchByName(firstName)
+    override fun getCustomersByFirstName(firstName: String): List<Customer> {
+        return this.customerRepository.searchByFirstName(firstName)
     }
 }
