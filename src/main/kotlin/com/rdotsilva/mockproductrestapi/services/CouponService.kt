943fb83597bcaf73667f530d66f88a6e4a4b7a22
RdotSilva <RyandotSilva@gmail.com>
2022-01-02 06:47:06 -0500

    Update CouponService interface

--- src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CouponService.kt
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CouponService.kt
@@ -1,4 +1,8 @@
 package com.rdotsilva.mockproductrestapi.services
 
+import com.rdotsilva.mockproductrestapi.models.Coupon
+
 interface CouponService {
+    fun addCoupon(coupon: Coupon)
+    fun removeCoupon(id: String)
 }
