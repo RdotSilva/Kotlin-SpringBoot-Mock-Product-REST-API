13cb240c90217be216f461e79223aa81487f7764
RdotSilva <RyandotSilva@gmail.com>
2022-01-02 06:48:33 -0500

    Add placeholder functions for CouponServiceImpl

---
+++ src/main/kotlin/com/rdotsilva/mockproductrestapi/services/CouponServiceImpl.kt
@@ -0,0 +1,14 @@
+package com.rdotsilva.mockproductrestapi.services
+
+import com.rdotsilva.mockproductrestapi.models.Coupon
+
+class CouponServiceImpl : CouponService {
+    override fun addCoupon(coupon: Coupon) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeCoupon(id: String) {
+        TODO("Not yet implemented")
+    }
+
+}
